dcb7a818a51e0ae10045a4199373d2ccd756f9e1
Restloud <loudclod@gmail.com>
2023-06-25 15:15:57 +0200

    complete day3/part2

--- AdventOfCode/2022/day3/day3.m
+++ AdventOfCode/2022/day3/day3.m
@@ -3,6 +3,17 @@ duplicates = blanks(height(backpack_contents));
 alphabet = 'abcdefghijklmnopqrstuvwxyz';
 priorities = 0;
 
+%find_duplicates
+find_groups(backpack_contents, duplicates, alphabet, priorities)
+
+%% part 1
+
+function find_duplicates
+backpack_contents = readtable("backpack_contents.xlsx", ReadVariableNames=false);
+duplicates = blanks(height(backpack_contents));
+alphabet = 'abcdefghijklmnopqrstuvwxyz';
+priorities = 0;
+
 for i = 1:height(backpack_contents)
     
     current_backpack = char(backpack_contents{i, 1});
@@ -41,4 +52,61 @@ for i = 1:strlength(duplicates)
 end
 
 disp(priorities)
+end
 
+%% part 2 
+
+function find_groups(backpack_contents, duplicates, alphabet, priorities)
+for i = 1:3:height(backpack_contents)
+    first_backpack = char(backpack_contents{i, 1});
+    second_backpack = char(backpack_contents{i+1, 1});
+    if strlength(first_backpack) >= strlength(second_backpack)
+        for ii = 1:strlength(first_backpack)
+            for jj = 1:strlength(second_backpack)
+                if first_backpack(ii) == second_backpack(jj)
+                    duplicates(i) = first_backpack(ii);
+                    break
+                end
+            end
+            if duplicates(i) ~= ' '
+                break
+            end
+        end
+    else
+        for ii = 1:strlength(second_backpack)
+            for jj = 1:strlength(first_backpack)
+                if second_backpack(ii) == first_backpack(jj)
+                    duplicates(i) = second_backpack(ii);
+                    break
+                end
+            end
+            if duplicates(i) ~= ' '
+                break
+            end
+        end
+    end
+end
+
+duplicates = duplicates(1:3:strlength(duplicates));
+
+for i = 1:strlength(duplicates)
+    found = false;
+    for j = 1:strlength(alphabet)
+        if lower(duplicates(i)) == alphabet(j)
+            if duplicates(i) == lower(duplicates(i))
+                priorities = priorities + j;
+                found = true;
+            else 
+                priorities = priorities + j + 26;
+                found = true;
+            end
+        end
+        if found
+            break
+        end
+    end
+end
+
+disp(priorities)
+
+end
